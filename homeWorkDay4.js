a3e919fa206cb9267d0a2ad3e965cac953c32336
ChuHoa <74419130+ChuHoa@users.noreply.github.com>
2020-11-24 20:54:24 +0700

    s

--- homeWorkDay4.js
+++
@@ -1,8 +0,0 @@
-//bai1
-function arraycheck(arr){
-    if (Array.isArray(arr)) {
-        return true;
-    }else{return false;}
-}
-console.log(arraycheck('w3resource'));
-console.log(arraycheck([1, 2, 4, 0]));
